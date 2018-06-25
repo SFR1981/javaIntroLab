6355c62ede3a3dd9bfdcbce75b79afaf5183ca50
Stephen Rooney <s.f.rooney@gmail.com>
2018-06-25 15:10:40 +0100

    added add

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    int num1;
+    int num2;
+
+    @Before
+    public void before(){
+        this.num1 = 3;
+        this.num2 = 2;
+    }
+
+
+
+    @Test
+    public void hasAdd(){
+        assertEquals(5, Calculator.add(num1, num2), 0.01);
+    }
+
+
+}
